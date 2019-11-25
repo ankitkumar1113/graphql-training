b7a0bfead11fd7a5240cde2395823700edf944f4
Ankit <ankit@askned.com>
2019-11-25 15:29:37 +0530

    day 1

---
+++ schema/schema.js
@@ -0,0 +1,50 @@
+const graphql = require('graphql')
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql
+
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+    { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+    { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+    { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+];
+
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString},
+    })
+})
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        books: {
+            type: new GraphQLList(BookType),
+            args: {},
+            resolve(parent, args){
+                // return books;
+                return books
+            }
+        }
+    }
+    
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+})
